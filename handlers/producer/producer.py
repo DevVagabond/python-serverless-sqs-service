324f8b6297b5ddcf83bcec2f37767bd3d89a8beb
Shawon Kanji <shawonkanji46@gmail.com>
2024-05-22 22:41:51 +0800

    Add SQS message sending capability to producer handler

--- handlers/producer/producer.py
+++ handlers/producer/producer.py
@@ -1,14 +1,22 @@
 import os
 
 from util.util import format_response
+import boto3
 
 
 def producer(event, context):
     print('Producer handler called')
     print('Event: {}'.format(event))
     headers = event.get('headers')
+    body = event.get('body')
     api_key = headers.get('x-api-key') if headers else None
     print('Headers: {}'.format(headers))
     if api_key != os.environ['API_KEY']:
         return format_response({"message": "Unauthorized"})
-    return format_response({"message": "Hello from Producer!"})
+    sqs = boto3.client('sqs')
+
+    sqs.send_message(
+        QueueUrl=os.environ['SQS_URL'],
+        MessageBody=body
+    )
+    return format_response({"message": "Message sent to SQS queue"})
